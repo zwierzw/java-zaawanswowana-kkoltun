2a68f99132368525305801de310107263bdcaea4
Wojtek <zwierzw@wp.pl>
2021-03-13 12:44:54 +0100

    dziedziczenie, kompozycja, abstrakcja

--- src/main/java/com/sda/vehicles/VehicleApp.java
+++ src/main/java/com/sda/vehicles/VehicleApp.java
@@ -2,24 +2,38 @@ package com.sda.vehicles;
 
 public class VehicleApp {
     public static void main(String[] args) {
-        Vehicle vehicle = new Vehicle();
-        vehicle.run(13);
+//        Vehicle vehicle = new Vehicle();
+//        vehicle.run(13);
+//
+//        Car car = new Car();
+//        car.run(120);
+//
+//        car.addPassenger();
+//        car.addPassenger();
+//
+//        car.removePassengers();
+//
+//        Motorcycle motorcycle = new Motorcycle();
 
+//        Vehicle vehicle2 = new Vehicle(4);
+//        vehicle2.run(68);
+//
+//        vehicle2 = new Car();
+//        vehicle2.run(100);
+//
+//        vehicle2 = new Motorcycle();
+//        vehicle2.run(150);
+
+        Vehicle vehicle = new Vehicle(4);
         Car car = new Car();
-        car.run(120);
-
-        car.addPassenger();
-        car.addPassenger();
-
-        car.removePassengers();
-
         Motorcycle motorcycle = new Motorcycle();
+        runThisVehicle(vehicle);
+        runThisVehicle(car);
+        runThisVehicle(motorcycle);
+    }
 
-        Vehicle vehicle2 = new Vehicle(4);
-        vehicle2 = new Car();
-        vehicle2.run(100);
 
-        vehicle2 = new Motorcycle();
-        vehicle2.run(150);
+    private static void runThisVehicle(Vehicle vehicle) {
+        vehicle.run(100);
     }
 }
