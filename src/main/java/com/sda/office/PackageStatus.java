63dad43911fd8b70c7156a4b01582c73cd1dc138
Wojtek <zwierzw@wp.pl>
2021-03-14 15:38:52 +0100

    enum metody sstatyczne i niestatyczne

--- src/main/java/com/sda/office/PackageStatus.java
+++ src/main/java/com/sda/office/PackageStatus.java
@@ -3,7 +3,7 @@ package com.sda.office;
 public enum PackageStatus {
     SENT("Wysłana"),
     TRAVELLING("Podróżuje"),
-    COURIER ("W doręczeniu"),
+    COURIER("W doręczeniu"),
     DELIVERED("Dostarczona"),
     LOST("Zagubiona"),
     RETURNED("Zwrócona"),
@@ -18,6 +18,22 @@ public enum PackageStatus {
     public String getDescription() {
         return description;
     }
+
+    //statyczna metoda w klasie enum
+    public static PackageStatus[] getErrorStatuses() {
+        return new PackageStatus[]{LOST, RETURNED};
+    }
+
+    // niestatyczna metoda w klasie enum
+    public boolean isError() {
+        if (this == LOST || this == RETURNED) {
+            return true;
+        } else {
+            return false;
+        }
+        //return this == LOST || this == RETURNED; --> najprostsza wersja
+    }
+
 
     @Override
     public String toString() {
--- src/main/java/com/sda/office/PostOfficeApp.java
+++ src/main/java/com/sda/office/PostOfficeApp.java
@@ -1,5 +1,8 @@
 package com.sda.office;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 public class PostOfficeApp {
     public static void main(String[] args) {
         PackageStatus sent = PackageStatus.SENT;
@@ -10,5 +13,33 @@ public class PostOfficeApp {
 
         paczka.setStatus(PackageStatus.LOST);
         System.out.println(paczka);
+
+        PackageStatus[] packageStatuses = PackageStatus.values();
+        for(PackageStatus packageStatus : packageStatuses){
+            System.out.println(packageStatus);
+        }
+
+        PackageStatus packageStatus = PackageStatus.valueOf("SENT");
+        System.out.println(packageStatus);
+
+        //przykłady użycia enuma
+        LocalDate data = LocalDate.of(1992, Month.DECEMBER,12);
+        Month september = Month.of(9);
+        System.out.println(data);
+        System.out.println(september);
+
+        //Użycie statycznej metody enum
+        PackageStatus[] errorStatuses = PackageStatus.getErrorStatuses();
+        for (PackageStatus packages: errorStatuses){
+            System.out.println(packages);
+        }
+
+        //Uzycie niestatycznej metody enum
+        System.out.println(PackageStatus.LOST.isError());
+        System.out.println(PackageStatus.DELIVERED.isError());
+        System.out.println(PackageStatus.RETURNED.isError());
+
+
+
     }
 }
