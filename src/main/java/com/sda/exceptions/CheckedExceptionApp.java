9e13131a34f9f5c289d73a5b430820661e90bf61
Wojtek <zwierzw@wp.pl>
2021-03-14 12:15:09 +0100

    Wyjątki Checked unchecked po poprawie przez trenera

--- src/main/java/com/sda/exceptions/CheckedExceptionApp.java
+++ src/main/java/com/sda/exceptions/CheckedExceptionApp.java
@@ -2,25 +2,25 @@ package com.sda.exceptions;
 
 import java.util.Scanner;
 
-public class ExceptionApp {
+public class CheckedExceptionApp {
     public static void main(String[] args) {
         try {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Please enter your name: ");
             String input = scanner.nextLine();
             greet(input);
-        } catch (IllegalArgumentException exception) {
-            System.out.println("Wystąpił błąd: " + exception.getMessage());
+        } catch (Exception exception) {
+            System.out.println("Wystąpił błąd " + exception.getMessage());
         }
         System.out.println("Zamykam program.");
     }
 
-    static void greet(String name) {
+    static void greet(String name) throws Exception {
         if (name == null || name.isEmpty()) {
-            throw new IllegalArgumentException("Imię jest puste");
+            throw new Exception("Imię jest puste");
         }
         if (name.length() == 1) {
-            throw new IllegalArgumentException("Imię jest zbyt krótkie");
+            throw new Exception("Imię jest zbyt krótkie");
         }
         System.out.println("Hello " + name);
     }
