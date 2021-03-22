24b502417cbe8180c2bc36b3da824de6bf44fc06
Wojtek <zwierzw@wp.pl>
2021-03-22 22:52:02 +0100

    Enum, zadanie 3, TemperatureConvert poprawnie

--- src/main/java/com/sda/enum_home/TemperatureConvert/TemperatureConvert.java
+++ src/main/java/com/sda/enum_home/TemperatureConvert/TemperatureConvert.java
@@ -1,38 +1,63 @@
 package com.sda.enum_home.TemperatureConvert;
 
-public enum TemperatureConvert implements Converter{
+public enum TemperatureConvert {
     C_F('C', 'F', new Converter() {
         @Override
         public float convert(float tempIn) {
-            return tempIn *9/5 + 32;
+            return tempIn * 9 / 5 + 32;
         }
     }),
 
-    C_K('C','K', new Converter(){
+    C_K('C', 'K', new Converter() {
         @Override
         public float convert(float tempIn) {
             return tempIn + 273.15f;
         }
     }),
-    K_C(),
-    F_C(),
-    F_K(),
-    K_F();
+    K_C('K', 'C', new Converter() {
+        @Override
+        public float convert(float tempIn) {
+            return tempIn - 273.15f;
+        }
+    }),
+    F_C('F', 'C', new Converter() {
+        @Override
+        public float convert(float tempIn) {
+            return (tempIn - 32) * 5 / 9;
+        }
+    }),
+    F_K('F', 'K', new Converter() {
+        @Override
+        public float convert(float tempIn) {
+            return (tempIn - 32) * 5 / 9 + 273.15f;
+        }
+    }),
+    K_F('K', 'F', new Converter() {
+        @Override
+        public float convert(float tempIn) {
+            return 1.8f * (tempIn - 273.15f) + 32;
+        }
+    });
 
-    private float tempIn;
-    private float tempOut;
+    private char tempIn;
+    private char tempOut;
     private Converter converter;
 
 
-    TemperatureConvert(float tempIn, float tempOut, Converter converter) {
+    TemperatureConvert(char tempIn, char tempOut, Converter converter) {
         this.tempIn = tempIn;
         this.tempOut = tempOut;
         this.converter = converter;
     }
 
 
-    @Override
-    public float convert(float tempIn) {
-        return 0;
+    public static void convertTemperature(char tempIn, char tempOut, float temp) {
+        for (TemperatureConvert tc : TemperatureConvert.values()) {
+            if (tempIn == tc.tempIn && tempOut == tc.tempOut) {
+                System.out.println(tc.converter.convert(temp));
+            }
+        }
+
+
     }
 }
---
+++ src/main/java/com/sda/enum_home/TemperatureConvert/TemperatureConverterApp.java
@@ -0,0 +1,8 @@
+package com.sda.enum_home.TemperatureConvert;
+
+public class TemperatureConverterApp {
+    public static void main(String[] args) {
+        TemperatureConvert.convertTemperature('C', 'K', 0.0f);
+        TemperatureConvert.convertTemperature('K', 'F', 20);
+    }
+}
